d5a43787289c7c0e025b62c8e05be25c76dd35cc
prosvirninav <68253400+prosvirninav@users.noreply.github.com>
2020-08-05 18:58:15 +0300

    Add files via upload

---
+++ Best-tour-plan/js/main.js
@@ -0,0 +1,29 @@
+var hotelSlider = new Swiper(".hotel-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
+  },
+});
+
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log(".клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
