52d41130d0301659f1524936be5ba8a26e070887
prosvirninav <prosvirninav@mail.ru>
2020-08-07 15:19:12 +0300

    fix: button close

--- Best-tour-plan/js/main.js
+++ Best-tour-plan/js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
   });
 
   var modalButton = $('[data-toggle="modal"]');
-  var closeModalButton = $('[modal__close]');
+  var closeModalButton = $('.modal-button__close');
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
